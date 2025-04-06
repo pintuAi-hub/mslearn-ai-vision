2ee3450466815c9291eb7028c2515fc17c0a139e
pintu3809 <pintu3809@yahoo.com>
2025-04-06 09:38:51 +0530

    testing for imageclasification done

--- Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
+++ Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
@@ -1,14 +1,10 @@
 using System;
 using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
-using System.Drawing;
 using Microsoft.Extensions.Configuration;
+using Azure.AI.Vision.ImageAnalysis;
 using Azure;
-
+using System.Drawing;
 // Import namespaces
 
 namespace image_analysis
@@ -34,6 +30,9 @@ namespace image_analysis
                 }
 
                 // Authenticate Azure AI Vision client
+                ImageAnalysisClient client = new ImageAnalysisClient(
+                                new Uri(aiSvcEndpoint),
+                                new AzureKeyCredential(aiSvcKey));
 
                 
                 // Analyze image
@@ -60,9 +59,78 @@ namespace image_analysis
                                                      FileMode.Open);
 
             // Get result with specified features to be retrieved
+            ImageAnalysisResult result = client.Analyze(
+                    BinaryData.FromStream(stream),
+                    VisualFeatures.Caption | 
+                    VisualFeatures.DenseCaptions |
+                    VisualFeatures.Objects |
+                    VisualFeatures.Tags |
+                    VisualFeatures.People);
             
             
             // Display analysis results
+            // Get image captions
+            if (result.Caption.Text != null)
+            {
+                Console.WriteLine(" Caption:");
+                Console.WriteLine($"   \"{result.Caption.Text}\", Confidence {result.Caption.Confidence:0.00}\n");
+            }
+
+            // Get image dense captions
+            Console.WriteLine(" Dense Captions:");
+            foreach (DenseCaption denseCaption in result.DenseCaptions.Values)
+            {
+                Console.WriteLine($"   Caption: '{denseCaption.Text}', Confidence: {denseCaption.Confidence:0.00}");
+            }
+            // Get image tags
+            if (result.Tags.Values.Count > 0)
+            {
+                Console.WriteLine($"\n Tags:");
+                foreach (DetectedTag tag in result.Tags.Values)
+                {
+                    Console.WriteLine($"   '{tag.Name}', Confidence: {tag.Confidence:F2}");
+                }
+            }
+                
+                // Get objects in the image
+                Console.WriteLine(result.Objects.Values.Count);
+            if(result.Objects.Values.Count > 0)
+            {
+
+                Console.WriteLine("objects");
+
+                //prepare image drawing
+                stream.Close();
+                 Image image = Image.FromFile(imageFile);
+                 Graphics graphics = Graphics.FromImage(image);
+                 Pen pen = new Pen(Color.Cyan, 3);
+                 Font font = new Font("Arial",16);
+                 SolidBrush solidBrush = new SolidBrush(Color.WhiteSmoke);
+
+                 foreach (DetectedObject detectedObject in result.Objects.Values)
+                {
+                    Console.WriteLine($"   \"{detectedObject.Tags[0].Name}\"");
+
+                    // Draw object bounding box
+                    var r = detectedObject.BoundingBox;
+                    Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                    graphics.DrawRectangle(pen, rect);
+                    graphics.DrawString(detectedObject.Tags[0].Name,font,solidBrush,(float)r.X, (float)r.Y);
+                }
+
+                // Save annotated image
+                String output_file = "objects.jpg";
+                image.Save(output_file);
+                Console.WriteLine("  Results saved in " + output_file + "\n");
+
+
+
+
+                
+            }
+            
+
+
             
 
         }
